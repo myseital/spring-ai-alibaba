c708089634017c22e67bac9cacd3ad731d0c5630
myseital <myseital24@gmail.com>
2026-03-13 21:41:44 +0800

    init

---
+++ spring-ai-chat/dashscope-chat/src/main/java/org/alibaba/cloud/ai/chat/dashscope/DashScopeChatModelApplication.java
@@ -0,0 +1,13 @@
+package org.alibaba.cloud.ai.chat.dashscope;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashScopeChatModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashScopeChatModelApplication.class, args);
+    }
+}
