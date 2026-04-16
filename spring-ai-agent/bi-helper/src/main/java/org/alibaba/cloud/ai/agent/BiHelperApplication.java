4e5b099d1ceed5d17697ab733237efdac04bfca4
myseital <myseital24@gmail.com>
2026-04-16 11:45:10 +0800

    add module bi-helper

---
+++ spring-ai-agent/bi-helper/src/main/java/org/alibaba/cloud/ai/agent/BiHelperApplication.java
@@ -0,0 +1,16 @@
+package org.alibaba.cloud.ai.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author myseital
+ * @date 2026/4/13
+ */
+@SpringBootApplication
+public class BiHelperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BiHelperApplication.class, args);
+    }
+}
